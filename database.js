f8cc02644fc5e1630ec77f1c89b25afe24498fcb
Brayan <brayantandap@gmail.com>
2024-01-29 15:43:38 -0500

    updated > database.js

---
+++ database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+mongoose.set('strict',false);
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI + "/emailAPI", {});
+        await mongoose.connection.db.collection('emailAPI').insertOne({ example: 'data' });
+        console.log("Conexión exitosa a la base de datos");
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports = connectToDatabase;
