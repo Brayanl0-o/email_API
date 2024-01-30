a8fda127b2172441485dc4d928913aa343fa8a24
Brayan <brayantandap@gmail.com>
2024-01-30 13:04:22 -0500

    updated > database.js

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ mongoose.set('strict',false);
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URI + "/emailAPI", {});
-        await mongoose.connection.db.collection('emailAPI').insertOne({ example: 'data' });
+        // await mongoose.connection.db.collection('emailAPI').insertOne({ example: 'data' });
         console.log("Conexión exitosa a la base de datos");
     } catch (err) {
         console.error(err);
