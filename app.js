807f84355a1fc320070c1b67904a9f9aef6f2dcd
Brayan <brayantandap@gmail.com>
2024-01-30 13:04:05 -0500

    updated > app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const connectToDatabase = require('./database');
+const bodyParser = require('body-parser');
+// const sendEmail = require('./routers/send-email')
+const receiveForm = require('./routers/receive-form')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
     origin:"*",
@@ -11,6 +17,8 @@ app.use(cors({
 app.get('/',(req,res)=> {
     res.send('Connected to Email API')
 })
+// app.use('/sendEmail', sendEmail);
+app.use('/receiveForm', receiveForm);
 
 connectToDatabase();
 module.exports = app;
