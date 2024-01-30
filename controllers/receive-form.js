3faf066bff1794cca00cff6c8154deee85696253
Brayan <brayantandap@gmail.com>
2024-01-30 13:04:49 -0500

    updated > receive-form.js(controller)

--- controllers/receive-form.js
+++ controllers/receive-form.js
@@ -1,25 +1,24 @@
 const Email = require('../models/email')
-const nodemailer = require('nodemailer')
+const send_email = require('./send-email')
 
 const receiveInfoFromForm = async (req,res) => {
     try{
         const {sendersName, emailAddress,phoneNumber, message} = req.body;
+        const newEmail = new Email({sendersName, emailAddress, phoneNumber, message});
+        await newEmail.save();
 
-        const newEmail = new Email({
+        const emailData = {
             sendersName,
             emailAddress,
             phoneNumber,
-            message,
-        });
-        await newEmail.save();
-        
-        res.status(200).json({message:'Datos recibidos correctamente '})
-    
+            message
+        };
+        // console.log(typeof controllerSendEmail.send_email); 
+        await send_email(emailData,res);
     }catch(error){
         console.error(error);
         res.status(500).json({ error: 'Error al procesar la solicitud.' });
     }
-
 };
 
 module.exports = { receiveInfoFromForm };
